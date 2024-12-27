44fb4ba6bba892a097ddc73f50830c241f6e6da2
Mohammed-Ashiek <150942526+Mohammed-Ashiek@users.noreply.github.com>
2024-12-27 16:05:14 +0530

    day6 loopTask2

---
+++ LoopTask2.java
@@ -0,0 +1,39 @@
+package day6;
+
+public class LoopTask2 {
+public static void main(String[] args) {
+	//Write a Java program to print all the numbers from 50 to 1 in reverse order using a for loop. 
+	
+	/*for (int i = 50; i >0 ; i--) {
+		System.out.println(i);*/
+	
+	//Write a program to calculate the sum of all odd numbers from 1 to 100. Display the result at the end.
+	
+	
+	/*int sum = 0 ;
+	for (int i = 0; i <=100 ; i++) {
+		if (i%2!=0) {
+			sum+=i;
+		}
+	}
+	System.out.println(sum);*/
+	
+	
+	/* Write a Java program that does the following: 
+1. Print all the numbers divisible by 3 from 1 to 30 using a for loop. 
+2. Display the count of such numbers at the end.*/
+	
+	/*int sum = 0 ;
+	for (int i = 1; i <=30 ; i++) {
+		if (i%3==0) {
+			System.out.println(i);
+			sum+=i;
+		}
+	}
+	System.out.println(sum);*/
+	
+	
+		
+	}
+}
+
