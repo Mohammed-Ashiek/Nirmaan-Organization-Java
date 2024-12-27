a2305d82d6539b140e165ff04a5d8d8687d7ab77
Mohammed-Ashiek <150942526+Mohammed-Ashiek@users.noreply.github.com>
2024-12-27 15:48:00 +0530

    day5 task2 in data types

---
+++ Task2DataTypes.java
@@ -0,0 +1,33 @@
+package day5;
+
+public class Task2DataTypes {
+public static void main(String[] args) {
+	 
+    String name1 = "Leo Das"; 
+    String name2 = "Sathya"; 
+    String adjective1 = "sleepy"; 
+    String adjective2 = "productive"; 
+    String noun1 = "kittens"; 
+    String noun2 = "pancakes"; 
+    String verb1 = "dance"; 
+    String noun3 = "drums"; 
+    String noun4 = "birds"; 
+    String adjective3 = "amused"; 
+    String place1 = "the moon"; 
+    String noun5 = "ice"; 
+    int number = 2077; 
+    String noun6 = "robots"; 
+
+    // Story 
+    String story = "This morning " + name1 + " woke up feeling " + adjective1 + ". " +
+                   "'It is going to be a " + adjective2 + " day!' Outside, a bunch of " + noun1 + "s were " +
+                   "protesting to keep " + noun2 + " in stores. They began to " + verb1 + " to the rhythm of " +
+                   "the " + noun3 + ", which made all the " + noun4 + "s very " + adjective3 + ". Concerned, " +
+                   name1 + " texted " + name2 + ", who flew " + name1 + " to " + place1 + " and dropped " +
+                   name1 + " in a puddle of frozen " + noun5 + ". " + name1 + " woke up in the year " + number + ", " +
+                   "in a world where " + noun6 + "s ruled the world.";
+
+   
+    System.out.println(story);
+}
+}
