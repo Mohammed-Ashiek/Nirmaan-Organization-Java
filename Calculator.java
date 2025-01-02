3841f5978c81d926428346aa8e5659a7b4536043
Mohammed-Ashiek <150942526+Mohammed-Ashiek@users.noreply.github.com>
2025-01-02 15:28:32 +0530

    day9 encapCalci

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+package day9;
+
+public class Calculator {
+int add(int firstNum, int secondNum) {
+	return firstNum + secondNum; 
+}
+
+int sub(int firstNum, int secondNum) {
+	return firstNum - secondNum; 
+}
+
+int multiply(int firstNum, int secondNum) {
+	return firstNum * secondNum; 
+}
+
+int divide(int firstNum, int secondNum) {
+	return firstNum / secondNum; 
+}
+
+int modulus(int firstNum, int secondNum) {
+	return firstNum % secondNum; 
+}
+
+
+
+
+}
